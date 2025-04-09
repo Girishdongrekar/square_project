33a320c1091299a928e8f26cd1484d89c8ef360e
Girish <girishdongrekar123@gmail.com>
2025-04-09 15:45:14 +0530

    adding CI -12

--- .github/workflows/_test.py
+++ .github/workflows/_test.py
@@ -3,13 +3,13 @@ from sq import square
 
 class TestSquareFunction(unittest.TestCase):
     def test_positive(self):
-        self.assertEqual(square(5), 25)
+        self.assertEqual(square(3), 9)
 
     def test_zero(self):
         self.assertEqual(square(0), 0)
 
     def test_negative(self):
-        self.assertEqual(square(-3), 9)
+        self.assertEqual(square(-2), 4)
 
 if __name__ == '__main__':
     unittest.main()
--- sq.py
+++ sq.py
@@ -3,10 +3,9 @@ import streamlit as st
 def square(n):
     return n * n
 
-st.title("🔢 Square Calculator")
-
-num = st.number_input("Enter a number", step=1, format="%d")
-
-if st.button("Calculate Square"):
-    result = square(num)
-    st.success(f"✅ The square of {num} is {result}")
+if __name__ == "__main__":
+    st.title("🔢 Square Calculator")
+    num = st.number_input("Enter a number", step=1, format="%d")
+    if st.button("Calculate Square"):
+        result = square(num)
+        st.success(f"✅ The square of {num} is {result}")
